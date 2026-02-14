61e4418d30b69ec41309b87b16e1f549532c1ce3
Harman Deep Singh <harmanthebest@gmail.com>
2026-02-14 20:20:56 +0530

    Added the reset command to reset database

--- main.go
+++ main.go
@@ -35,8 +35,10 @@ func main() {
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
+
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
 
 	if len(os.Args) < 2 {
 		log.Fatalf("Usage: cli <command> [args...]")
